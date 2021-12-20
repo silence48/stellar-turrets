cbbc6410afda77598177831b90b34a0774fcd05a
silence48 <35656692+silence48@users.noreply.github.com>
2021-12-20 06:37:11 -0500

    add authtoken generator to contracts

---
+++ wrangler/contracts/authtoken.js
@@ -0,0 +1,42 @@
+// This script will create the xdr needed for an auth token.  make sure to modify it as needed.
+
+const { Keypair, TransactionBuilder, Server, Account, Networks, Operation } = require('stellar-sdk')
+
+// The hashes the fee payment can apply to
+// Note - this can be empty. Then, this key can be used to run any txFunction.
+const txFunctionHashes = [
+];
+
+const privateKeypair = Keypair.fromSecret('//putyoursecrethere');
+const pk = privateKeypair.publicKey();
+
+const testnet = new Server('https://horizon-testnet.stellar.org');
+(async () => {
+    try {
+        // setup a fake account with a -1 seq number.
+        // This ensures a zero seq number when the transaction is built (TransactionBuilder increments once).
+        const tempAcct = new Account(pk, '-1');
+        const fee = await testnet.fetchBaseFee();
+        const txBuilder = new TransactionBuilder(tempAcct, {fee, networkPassphrase: Networks.TESTNET});
+        
+        // add the manage data operations to specify the allowed txHashes to be run for this user
+        for (const hash of txFunctionHashes) {
+            txBuilder.addOperation(Operation.manageData({
+                name: "txFunctionHash",
+                value: hash
+            }));
+        }
+        
+        // set TTL on the token for 1 hour
+        const tx = txBuilder.setTimeout(24*60*60).build();
+        
+        // sign the TX with the source account of the Transaction. This token is now valid for this public key!
+        tx.sign(privateKeypair);
+        
+        // this is the XDR Token
+        const token = tx.toEnvelope().toXDR('base64')
+        console.log(token);
+    } catch (e) {
+        console.error(e);
+    }
+})();
